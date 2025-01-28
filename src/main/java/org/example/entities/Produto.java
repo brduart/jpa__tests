6b4480ab9ea583c9efea4ba4935a51b37c61b076
= <=>
2025-01-28 08:58:27 -0300

    new entitie

---
+++ src/main/java/org/example/entities/Produto.java
@@ -0,0 +1,61 @@
+package org.example.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String descricao;
+    private BigDecimal preco;
+
+    public Produto() {
+
+    }
+
+    public Produto(Long id, String name, String descricao, BigDecimal preco) {
+        this.id = id;
+        this.name = name;
+        this.descricao = descricao;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+}
